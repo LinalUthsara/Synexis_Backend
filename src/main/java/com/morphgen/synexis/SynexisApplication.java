77b57c0ceb486dea493cea4741bccccedb1364dd
Perera L.P.L.U. <cb8b11075@kiu.ac.lk>
2025-05-05 09:07:32 +0530

    initialize: project synexis

---
+++ src/main/java/com/morphgen/synexis/SynexisApplication.java
@@ -0,0 +1,13 @@
+package com.morphgen.synexis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynexisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynexisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morphgen/synexis/SynexisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morphgen.synexis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynexisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
